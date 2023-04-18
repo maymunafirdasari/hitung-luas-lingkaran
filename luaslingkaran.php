6f891b37205dfaafc0a22bf86abbc80dad3f343e
maymunafirdasari <124543020+maymunafirdasari@users.noreply.github.com>
2023-04-18 10:09:01 +0700

    Add files via upload

---
+++ luaslingkaran.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+       <title>Luas Lingkaran</title> 
+    </head>
+    <body>
+        <h1>Hitung Luas Lingkaran</h1>
+        <h2>Isi Data :</h2>
+<form action:"./" method="POST" enctype="multipart/form-data">
+        <tabel>
+        <tr>
+            <td>Jari-jari</td>
+            <td>:</td>
+            <td><input type="text" name="jari" required></td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td><input type="submit" name="submit" value="Hitung"></td>
+</tr>
+</tabel>
+</form>
+
+<?php
+if(isset($_POST['submit'])){
+    $jari = $_POST['jari'];
+    $phie = 22/7;
+
+    // menghitung luas lingkaran
+    $luas_lingkaran = $phie*($jari*$jari);
+
+    echo "Diketahui :<br/>";
+    echo "Jari-jari = $jari<br />";
+    echo "Phie = $phie<br />";
+    echo "Maka Luas Lingkaran adalah [$phie x $jari x $jari] = $luas_lingkaran";
+}
+ ?>
+ </body>
+ </html>
